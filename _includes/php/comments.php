12dfe9814b7ad96f91eff69e914321b27a82b254
Corey Chambers <coreychambers123@gmail.com>
2019-03-11 18:18:41 -0400

    Create comments.php

---
+++ _includes/php/comments.php
@@ -0,0 +1,27 @@
+// Bad example
+<?php
+
+   /*
+
+   Riftside PHP Riot API
+
+   */
+
+   class riotapi {
+   
+   }
+   
+// Good example
+<?php
+
+   /**
+    * Grab a list of the current logged in user's friends
+    * @param integer $user_id
+    * @return string $friends
+    */
+   
+   function get_friends($user_id) {
+      ...
+      
+      return $friends;
+   }
